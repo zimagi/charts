e25272269dee1ee9872507f6e13c12d3a21c69a1
Erik Jagyugya <jagyugyaerik@gmail.com>
2021-08-02 21:00:34 +0200

    Imrpove releasing cycle
    Signed-off-by: Erik Jagyugya <jagyugyaerik@gmail.com>

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -12,42 +11,41 @@ set -o pipefail
 : "${GIT_EMAIL:?Environment variable GIT_EMAIL must be set}"
 : "${GIT_REPOSITORY_NAME:?Environment variable GIT_REPOSITORY_NAME must be set}"
 
-readonly REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
 
 main() {
-    pushd "$REPO_ROOT" > /dev/null
+    local charts_dir=charts
 
-    echo "Fetching tags..."
-    git fetch --tags
+    local repo_root
+    repo_root="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
+    pushd "${repo_root}" > /dev/null
 
+    echo "Looking up latest tag..."
     local latest_tag
     latest_tag=$(find_latest_tag)
-
-    local latest_tag_rev
-    latest_tag_rev=$(git rev-parse --verify "$latest_tag")
-    echo "$latest_tag_rev $latest_tag (latest tag)"
 
     local head_rev
     head_rev=$(git rev-parse --verify HEAD)
     echo "$head_rev HEAD"
 
-    if [[ "$latest_tag_rev" == "$head_rev" ]]; then
-        echo "No code changes. Nothing to release."
-        exit
-    fi
-
-    rm -rf .deploy
-    mkdir -p .deploy
-
     echo "Identifying changed charts since tag '$latest_tag'..."
-
     local changed_charts=()
-    readarray -t changed_charts <<< "$(git diff --find-renames --name-only "$latest_tag_rev" -- charts | cut -d '/' -f 2 | uniq)"
+    readarray -t changed_charts <<< "$(get_changed_charts "$latest_tag")"
 
     if [[ -n "${changed_charts[*]}" ]]; then
+        # install_chart_releaser
+
+        rm -rf .cr-release-packages
+        mkdir -p .cr-release-packages
+
+        rm -rf .cr-index
+        mkdir -p .cr-index
+
         for chart in "${changed_charts[@]}"; do
-            echo "Packaging chart '$chart'..."
-            package_chart "charts/$chart"
+            if [[ -d "$chart" ]]; then
+                package_chart "$chart"
+            else
+                echo "Chart '$chart' no longer exists in repo. Skipping it..."
+            fi
         done
 
         release_charts
@@ -60,49 +58,71 @@ main() {
     popd > /dev/null
 }
 
+get_changed_charts() {
+    local commit="$1"
+
+    local changed_files
+    changed_files=$(git diff --find-renames --name-only "$commit" -- "$charts_dir")
+
+    local depth=$(( $(tr "/" "\n" <<< "$charts_dir" | sed '/^\(\.\)*$/d' | wc -l) + 1 ))
+    local fields="1-${depth}"
+
+    cut -d '/' -f "$fields" <<< "$changed_files" | uniq | filter_charts
+}
+
+filter_charts(){
+    while read -r chart; do
+        [[ ! -d "$chart" ]] && continue
+        local file="$chart/Chart.yaml"
+        if [[ -f "$file" ]]; then
+            echo "$chart"
+        else
+           echo "WARNING: $file is missing, assuming that '$chart' is not a Helm chart. Skipping." 1>&2
+        fi
+    done
+}
+
+package_chart(){
+    local chart="$1"
+
+    local args=("$chart" --package-path .cr-release-packages)
+
+    echo "Packaging chart '$chart'..."
+    chart-releaser package "${args[@]}"
+}
+
 find_latest_tag() {
     if ! git describe --tags --abbrev=0 2> /dev/null; then
         git rev-list --max-parents=0 --first-parent HEAD
     fi
 }
 
-package_chart() {
-    local chart="$1"
-    helm dependency build "$chart"
-    helm package "$chart" --destination .deploy
-}
-
 release_charts() {
-    chart-releaser upload -o "$GIT_USERNAME" -r "$GIT_REPOSITORY_NAME" -p .deploy -t "$GH_TOKEN"
+    local args=(-o "$GIT_USERNAME" -r "$GIT_REPOSITORY_NAME" -c "$(git rev-parse HEAD)" -t "$GH_TOKEN")
+
+    echo 'Releasing charts...'
+    chart-releaser upload "${args[@]}"
 }
 
 update_index() {
-    chart-releaser index -i .deploy/index.yaml -o "$GIT_USERNAME" -r "$GIT_REPOSITORY_NAME" -p .deploy/index.yaml -c "https://zimagi.github.io/charts/"
+    chart-releaser index -o "$GIT_USERNAME" -r "$GIT_REPOSITORY_NAME" -c "https://jagyugyaerik.github.io/cr--test/"
+    cat .cr-index/index.yaml
 
     git config user.email "$GIT_EMAIL"
     git config user.name "$GIT_USERNAME"
 
-    for file in charts/*/*.md; do
-        if [[ -e $file ]]; then
-            mkdir -p ".deploy/docs/$(dirname "$file")"
-            cp --force "$file" ".deploy/docs/$(dirname "$file")"
-        fi
-    done
-
     git checkout gh-pages
-    cp --force .deploy/index.yaml index.yaml
+    cp --force .cr-index/index.yaml index.yaml
 
-    if [[ -e ".deploy/docs/charts" ]]; then
-        mkdir -p charts
-        cp --force --recursive .deploy/docs/charts/* charts/
-    fi
-
-    git checkout master -- README.md
+    git status
+    ls -la
+    cat index.yaml
 
     if ! git diff --quiet; then
         git add .
         git commit --message="Update index.yaml" --signoff
-        git push "$GIT_REPOSITORY_URL" gh-pages
+        git push -q https://"${GH_TOKEN}"@github.com/"${GIT_USERNAME}"/"${GIT_REPOSITORY_NAME}".git  gh-pages
+
     fi
 }
 
